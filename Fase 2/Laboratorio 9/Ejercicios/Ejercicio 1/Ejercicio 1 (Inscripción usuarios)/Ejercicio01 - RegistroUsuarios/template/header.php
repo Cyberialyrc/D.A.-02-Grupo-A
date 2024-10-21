0c6c803497bbaa806ac6155ceffd181199f70c0b
FrostGZ <velociraptoruhc@gmail.com>
2024-10-21 17:54:38 -0500

    Ejercicio 01 - Inscripción de usuarios

---
+++ Fase 2/Laboratorio 9/Ejercicios/Ejercicio 1/Ejercicio 1 (Inscripción usuarios)/Ejercicio01 - RegistroUsuarios/template/header.php
@@ -0,0 +1,21 @@
+<!-----------------------------------------------------------------------------------
+ARCHIVO: header.php
+AUTOR: Manrique Silva Ricardo Gabriel
+FECHA: 21/10/24
+DESCRIPCIÓN: Archivo que se encarga del encabezado de las páginas
+------------------------------------------------------------------------------------>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscripción de Usuarios</title>
+    <link rel="stylesheet" href="/Ejercicio01 - RegistroUsuarios/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Ejercicio01 - RegistroUsuarios/css/estilos.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
+        <a class="navbar-brand" href="#">Registro de Usuarios</a>
+    </nav>
+</body>
+</html>
